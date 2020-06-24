436c88a48e5e03a2abf1b8c55f947f61168ed99c
Fritzent <afritzent@gmail.com>
2020-06-24 12:41:51 +0700

    Add Splash Screen in Mainactivity

--- app/src/main/java/com/example/binarcoursepengenalan/MainActivity.kt
+++ app/src/main/java/com/example/binarcoursepengenalan/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.binarcoursepengenalan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ini untuk ngehide actionbar default dari android studio
+        supportActionBar?.hide()
+
+        //ini untuk ngehubungin xml ke kotlinnya
+        val qoutesText = findViewById<TextView>(R.id.QuotesText);
+        val qoutesPerson = findViewById<TextView>(R.id.QuotesPerson);
+
+        //ini buat ngatur textview nya
+        qoutesText.setText("\"Action is the foundational key to all success.\"")
+        qoutesPerson.setText("~Pablo Picasso")
+
+        fun newFunction(){
+            //ini new function
+            //ini cari untuk membuat sebuah function baru di kotlin
+        }
+
     }
 }
